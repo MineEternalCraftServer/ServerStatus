346aa999058a463efa63b8e8e8c19f44d0d65b8b
Chiharu-Hagihara <chiharuhagihara803@gmail.com>
2021-01-26 14:48:26 +0900

    refactor: optimize comments

--- src/main/java/server/mecs/serverstatus/paper/bungee/ServerStatus.java
+++ src/main/java/server/mecs/serverstatus/paper/bungee/ServerStatus.java
@@ -3,4 +3,12 @@ package server.mecs.serverstatus.paper.bungee;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class ServerStatus extends Plugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
 }
