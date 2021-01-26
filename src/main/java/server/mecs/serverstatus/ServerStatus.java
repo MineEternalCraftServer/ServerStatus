efa63ad982fb0c07da5bb8c100b82989a883c55e
Chiharu-Hagihara <chiharuhagihara803@gmail.com>
2021-01-26 14:38:13 +0900

    initial commit

---
+++ src/main/java/server/mecs/serverstatus/ServerStatus.java
@@ -0,0 +1,17 @@
+package server.mecs.serverstatus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServerStatus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
